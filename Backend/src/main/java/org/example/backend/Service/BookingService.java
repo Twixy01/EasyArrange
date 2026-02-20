2adcb8a844788e2a310d7b43a3f25adc37311743
peter.toth <toth.peter.0605@gmail.com>
2026-02-20 19:40:57 +0100

    Add service layer to LoginServlet

--- Backend/src/main/java/org/example/backend/Service/BookingService.java
+++ Backend/src/main/java/org/example/backend/Service/BookingService.java
@@ -1,5 +1,8 @@
 package org.example.backend.Service;
 
+import org.example.backend.Model.entity.Booking;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingService {
@@ -13,12 +16,12 @@ public interface BookingService {
     List<Booking> getBookingsByCustomerId(long customerId) throws Exception;
 
     List<Booking> getBookingsBetween(
-            java.time.LocalDateTime start, java.time.LocalDateTime end) throws Exception;
+            LocalDateTime start, LocalDateTime end) throws Exception;
 
     List<Booking> getBookingsByStaffBetween(
-            long staffId, java.time.LocalDateTime start, java.time.LocalDateTime end) throws Exception;
+            long staffId, LocalDateTime start, LocalDateTime end) throws Exception;
 
     List<Booking> getBookingsByCustomerBetween(
-            long customerId, java.time.LocalDateTime start, java.time.LocalDateTime end) throws Exception;
+            long customerId, LocalDateTime start, LocalDateTime end) throws Exception;
 
 }
