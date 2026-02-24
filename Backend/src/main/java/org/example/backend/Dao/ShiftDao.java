01ecba62bcc6136ca0d13d2b40e27462bb8f5e84
peter.toth <toth.peter.0605@gmail.com>
2026-02-24 15:19:30 +0100

    Rename shift dao jpa methods

--- Backend/src/main/java/org/example/backend/Dao/ShiftDao.java
+++ Backend/src/main/java/org/example/backend/Dao/ShiftDao.java
@@ -9,5 +9,5 @@ public interface ShiftDao extends Dao<Shift> {
     List<Shift> findAllShiftsByStaffId(long staffId);
     List<Shift> findAllShiftsByTime(LocalTime time);
     List<Shift> findShiftsBetweenTime(LocalTime startTime, LocalTime endTime);
-    List<Shift> findShiftsByStaffIdBetweenDates(long staff_id, LocalTime startTime, LocalTime endTime);
+    List<Shift> findShiftsByStaffIdBetweenTimes(long staff_id, LocalTime startTime, LocalTime endTime);
 }
