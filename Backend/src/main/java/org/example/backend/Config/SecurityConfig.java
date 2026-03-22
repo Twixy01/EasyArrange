9785ed3b41185316c10d121344b0246055672ef9
peter.toth <toth.peter.0605@gmail.com>
2026-03-22 11:50:16 +0100

    Add basic Exception handler

--- Backend/src/main/java/org/example/backend/Config/SecurityConfig.java
+++ Backend/src/main/java/org/example/backend/Config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -85,10 +86,11 @@ public class SecurityConfig {
 //			.requestMatchers(HttpMethod.POST,"/api/register").hasRole("ADMIN")
 //			.requestMatchers(HttpMethod.PUT,"/api/users/**").hasRole("ADMIN")
 //			.requestMatchers(HttpMethod.DELETE,"/api/users/**").hasRole("ADMIN")
-        ).formLogin(withDefaults());
+        );
 
         http.httpBasic(withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
