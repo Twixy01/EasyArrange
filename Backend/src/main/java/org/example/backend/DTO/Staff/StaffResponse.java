8e0cb1ef6506b0d1b90acac22bf5214b864ed36a
peter.toth <toth.peter.0605@gmail.com>
2026-04-07 09:10:12 +0200

    Added services to Staff Dto

--- Backend/src/main/java/org/example/backend/DTO/Staff/StaffResponse.java
+++ Backend/src/main/java/org/example/backend/DTO/Staff/StaffResponse.java
@@ -1,11 +1,15 @@
 package org.example.backend.DTO.Staff;
 
+import org.example.backend.DTO.Service.ServiceResponse;
 import org.example.backend.DTO.User.UserResponse;
+
+import java.util.List;
 
 public record StaffResponse(
         Long staffId,
         UserResponse user,
         String title,
-        String bio
+        String bio,
+        List<ServiceResponse> services
 ) {
 }
